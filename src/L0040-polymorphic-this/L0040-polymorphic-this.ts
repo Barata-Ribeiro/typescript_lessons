eb69ef2ee78fa185c36de5618e7209e8234bbbc0
João M J Barata Ribeiro <j.mendes1920@gmail.com>
2023-06-21 06:59:49 -0300

    'Gangs of Four' builder of polymorphic this

--- src/L0040-polymorphic-this/L0040-polymorphic-this.ts
+++ src/L0040-polymorphic-this/L0040-polymorphic-this.ts
@@ -32,3 +32,26 @@ export class SubCalculator extends Calculator {
 const calculator = new SubCalculator(10);
 calculator.add(5).mul(2).div(2).sub(5).pow(2);
 console.log(calculator);
+
+// Builder - GoF
+export class RequestBuilder {
+    private method: 'get' | 'post' | null = null;
+    private url: string | null = null;
+
+    setMethod(method: 'get' | 'post'): this {
+        this.method = method;
+        return this;
+    }
+
+    setUrl(url: string): this {
+        this.url = url;
+        return this;
+    }
+
+    send(): void {
+        console.log(`Sending data through '${this.method}' request to ${this.url}`);
+    }
+}
+
+const request = new RequestBuilder(); // Builder
+request.setUrl('https://www.google.com/').setMethod('post').send();
