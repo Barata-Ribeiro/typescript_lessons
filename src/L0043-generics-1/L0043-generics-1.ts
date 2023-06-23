119bb5de4c8a2570e5bf785d7242e1d99746d20c
João M J Barata Ribeiro <j.mendes1920@gmail.com>
2023-06-23 06:17:16 -0300

    Generics 1-2

--- src/L0043-generics-1/L0043-generics-1.ts
+++ src/L0043-generics-1/L0043-generics-1.ts
@@ -1,6 +1,6 @@
-type FilterCallback = (value: unknown, index?: number, array?: unknown[]) => boolean;
+type FilterCallback<U> = (value: U, index?: number, array?: U[]) => boolean;
 
-export function myFilter(array: unknown[], callbackfn: FilterCallback): unknown[] {
+export function myFilter<T>(array: T[], callbackfn: FilterCallback<T>): T[] {
     const newArray = [];
 
     for (let i = 0; i < array.length; i++) {
@@ -17,8 +17,5 @@ const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 const originalFilteredArray = array.filter((value) => value < 5);
 console.log(originalFilteredArray);
 
-const myFilteredArray = myFilter(array, (value) => {
-    if (typeof value === 'number') return value < 5;
-    return false;
-});
+const myFilteredArray = myFilter(array, (value) => value < 5);
 console.log(myFilteredArray);
